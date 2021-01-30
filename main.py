51ce189897c4e932e9a39f77c8d3c1d74330d1de
Bruno <david.lopez_pe@uao.edu.co>
2021-01-29 20:38:06 -0500

    patalla de login 100% terminada

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import eel
 
 eel.init('web')
-eel.start('index.html')
+eel.start('login.html')
---
+++ web/js/login.js
@@ -0,0 +1,27 @@
+function logIn(){
+    let email = document.getElementById("email").value
+    let password = document.getElementById("password").value
+    //si no ingresa los datos
+    if(email === "" || password === "") return document.getElementById("validationError").innerHTML = "Llena ambos campos para poder iniciar sesion";
+    const url = 'http://localhost:3000/login'
+    $.get(url, {Correo: email, Contrasena: password, Type: 'normal'}, (data)=>{
+        //si la contraseña es incorrecta
+        if(data === "wrong password") return  document.getElementById("validationError").innerHTML = "La contraseña que ingresaste es incorrecta. Inténtalo de nuevo";
+        //si el usuario no esta registrado
+        if(data === "no found") return document.getElementById("validationError").innerHTML = "El usuario que ingresaste no está registrado. Inténtalo de nuevo";
+        //si no hay errores
+        sessionStorage.setItem("user", JSON.stringify(data));
+        window.location.href = "index.html";
+    }).fail(_=>{
+        //si el servidor no responde
+        document.getElementById("validationError").innerHTML = "Error de conexión. Inténtalo más tarde";
+    });
+}
+
+//evento al presionar enter
+$("#email").keyup((event)=>{
+    if(event.keyCode == 13) $('#logInButton').click()
+})
+$("#password").keyup((event)=>{
+    if(event.keyCode == 13) $('#logInButton').click()
+})
