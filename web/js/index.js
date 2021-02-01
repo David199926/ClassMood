d08d37db8c7a2e45dfcddbae9c43d462a306e52d
Bruno <david.lopez_pe@uao.edu.co>
2021-02-01 16:03:14 -0500

    se consultan y muestran las proximas sesiones

--- web/js/index.js
+++ web/js/index.js
@@ -3,8 +3,8 @@ let user = JSON.parse(sessionStorage.getItem("user"));
 //setear el nombre del usuario en el menu lateral
 document.getElementById("userName").innerHTML = user.Nombre+' '+user.Apellido;
 //obtener la sesión actual (si existe)
-let url = 'http://localhost:3000/available';
-$.get(url, { Correo: user.Correo }, (data)=>{
+let url = 'http://localhost:3000/';
+$.get(url + 'available', { Correo: user.Correo }, (data)=>{
     if(data.length === 0){
         document.getElementById("mainContainer").innerHTML = `
         <div class="o-nosession-content">
@@ -18,16 +18,39 @@ $.get(url, { Correo: user.Correo }, (data)=>{
             let session = data[0];
             document.getElementById("titleDetection").innerHTML = session.NombreCurso;
             document.getElementById("groupDetection").innerHTML = `Grupo ${session.NumeroGrupo}`;
-            let horaInicio = new Date(session.HoraInicio);
-            let horaFinal = new Date(session.HoraFinal);
-            console.log(horaInicio)
-            document.getElementById("currentSessionStartTime").innerHTML = `${horaInicio.getHours() % 12 || 12}:${horaInicio.getMinutes().toString().padStart(2, '0')} ${horaInicio.getHours()> 12? 'PM': 'AM'}`;
-            document.getElementById("currentSessionEndTime").innerHTML = `${horaFinal.getHours() % 12 || 12}:${horaFinal.getMinutes().toString().padStart(2, '0')} ${horaFinal.getHours()> 12? 'PM': 'AM'}`;
+            document.getElementById("currentSessionStartTime").innerHTML = timeFormatter(new Date(session.HoraInicio));
+            document.getElementById("currentSessionEndTime").innerHTML = timeFormatter(new Date(session.HoraFinal));
         })
         
     }
+});
+//obtener las proximas sesiones
+$.get(url + 'upcoming', { Correo: user.Correo }, (data)=>{
+    if(data.length === 0){
+        $("#nextSessionsContainer").append('<span class="o-no-next-sessions">No hay sesiones agendadas</span>')
+    }
+    else{
+        console.log(typeof data[0].HoraInicio)
+        for(session of data){
+            $("#nextSessionsContainer").append(`
+                <div class="o-session">
+                    <div class="o-session-header">
+                        <span class="o-session-title" id="sessionGroupName">${session.NombreCurso}</span>
+                        <span class="o-session-group" id="sessionGroupNumber">Grupo ${session.NumeroGrupo}</span>
+                    </div>
+                    <div class="o-session-time">
+                        <span>Inicio:&nbsp;<span id="sessionStartTime">${timeFormatter(new Date(session.HoraInicio))}</span></span>
+                        <span>Fin:&nbsp;<span id="sessioneNDTime">${timeFormatter(new Date(session.HoraFinal))}</span></span>
+                    </div>
+                </div>
+            `)
+        }
+    }
 })
 
+function timeFormatter(time){
+    return `${time.getHours() % 12 || 12}:${time.getMinutes().toString().padStart(2, '0')} ${time.getHours()> 12? 'PM': 'AM'}`
+}
 
 function logOut(){
     sessionStorage.removeItem("user");
