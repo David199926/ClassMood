07079f2859033ef7f1e7157f1301493451343898
Bruno <david.lopez_pe@uao.edu.co>
2021-01-31 21:54:35 -0500

    d

---
+++ Configuration.py
@@ -0,0 +1,13 @@
+import eel
+import json
+CONF_FILE = "conf.json"
+
+@eel.expose
+def readConf():
+    with open(CONF_FILE, 'r') as conf:
+        return json.load(conf)
+
+@eel.expose
+def saveConf(data):
+    with open(CONF_FILE, 'w') as conf:
+        json.dump(data, conf, ensure_ascii= False)
--- main.py
+++ main.py
@@ -1,17 +1,6 @@
 import eel
-import json
+import Configuration
 
-CONF_FILE = "conf.json"
-
-@eel.expose
-def readConf():
-    with open(CONF_FILE, 'r') as conf:
-        return json.load(conf)
-
-@eel.expose
-def saveConf(data):
-    with open(CONF_FILE, 'w') as conf:
-        json.dump(data, conf, ensure_ascii= False)
 
 if __name__ == "__main__":
     eel.init('web')
