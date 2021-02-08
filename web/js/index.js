b5de65e49f545695216c7bc76ec9e2676273a8bd
Bruno <david.lopez_pe@uao.edu.co>
2021-02-08 16:18:03 -0500

    d

--- web/js/index.js
+++ web/js/index.js
@@ -1,39 +1,47 @@
-//obtener el usuario
-let user = JSON.parse(sessionStorage.getItem("user"));
-//setear el nombre del usuario en el menu lateral
-document.getElementById("userName").innerHTML = user.Nombre + ' ' + user.Apellido;
-//obtener la sesión actual (si existe)
-let url = 'https://classmood-appserver.herokuapp.com/';
-$.get(url + 'available', { Correo: user.Correo }, (data) => {
-    if (data.length === 0) {
-        document.getElementById("mainContainer").innerHTML = `
+/**
+ * funcion para obtener la sesion actual (si existe)
+ * @param {object} user 
+ */
+function getCurrentSession(user) {
+    //obtener la sesión actual (si existe)
+    let url = 'https://classmood-appserver.herokuapp.com/available';
+    $.get(url, { Correo: user.Correo }, (data) => {
+        if (data.length === 0) {
+            document.getElementById("mainContainer").innerHTML = `
         <div class="o-nosession-content">
             <span>No hay sesiones disponibles</span>
             <img src="./src/NoSession.png" alt="NoSession">
         </div>`;
-    }
-    else {
-        $('#mainContainer').load('deteccion.html', _ => {
-            //insertar informacion de la sesion
-            let session = data[0];
-            document.getElementById("titleDetection").innerHTML = session.NombreCurso;
-            document.getElementById("groupDetection").innerHTML = `Grupo ${session.NumeroGrupo}`;
-            document.getElementById('sessionDuration').innerHTML = `${timeFormatter(new Date(session.HoraInicio))}--${timeFormatter(new Date(session.HoraFinal))}`;
-            sessionStorage.setItem('currentSession', JSON.stringify(session))
-            getDevices()
-            eel.startTransmition(false)
-        })
+        }
+        else {
+            $('#mainContainer').load('deteccion.html', _ => {
+                //insertar informacion de la sesion
+                let session = data[0];
+                document.getElementById("titleDetection").innerHTML = session.NombreCurso;
+                document.getElementById("groupDetection").innerHTML = `Grupo ${session.NumeroGrupo}`;
+                document.getElementById('sessionDuration').innerHTML = `${timeFormatter(new Date(session.HoraInicio))}--${timeFormatter(new Date(session.HoraFinal))}`;
+                sessionStorage.setItem('currentSession', JSON.stringify(session))
+                getDevices()
+                eel.startTransmition(false)
+            })
 
-    }
-});
-//obtener las proximas sesiones
-$.get(url + 'upcoming', { Correo: user.Correo }, (data) => {
-    if (data.length === 0) {
-        $("#nextSessionsContainer").append('<span class="o-no-next-sessions">No hay sesiones agendadas</span>')
-    }
-    else {
-        for (session of data) {
-            $("#nextSessionsContainer").append(`
+        }
+    });
+}
+
+/**
+ * funcion para obtener las sesiones próximas (si existen)
+ * @param {object} user 
+ */
+function getUpcomingSessions(user) {
+    let url = 'https://classmood-appserver.herokuapp.com/upcoming'
+    $.get(url, { Correo: user.Correo }, (data) => {
+        if (data.length === 0) {
+            $("#nextSessionsContainer").append('<span class="o-no-next-sessions">No hay sesiones agendadas</span>')
+        }
+        else {
+            for (session of data) {
+                $("#nextSessionsContainer").append(`
                 <div class="o-session">
                     <div class="o-session-header">
                         <span class="o-session-title" id="sessionGroupName">${session.NombreCurso}</span>
@@ -45,9 +53,17 @@ $.get(url + 'upcoming', { Correo: user.Correo }, (data) => {
                     </div>
                 </div>
             `)
+            }
         }
-    }
-})
+    })
+}
+
+//obtener el usuario
+let user = JSON.parse(sessionStorage.getItem("user"));
+//setear el nombre del usuario en el menu lateral
+document.getElementById("userName").innerHTML = user.Nombre + ' ' + user.Apellido;
+getCurrentSession(user);
+getUpcomingSessions(user);
 
 /**
  * funcion para dar formato a los objetos Date
