87f3396405d825141d2c5dd5741379660704c4fa
Bruno <david.lopez_pe@uao.edu.co>
2021-02-08 21:56:14 -0500

    actualizacion automatica cuando se acaba una sesion

--- web/js/index.js
+++ web/js/index.js
@@ -8,21 +8,29 @@ function getCurrentSession(user) {
     $.get(url, { Correo: user.Correo }, (data) => {
         if (data.length === 0) {
             document.getElementById("mainContainer").innerHTML = `
-            <div class="o-nosession-content">
-                <span>No hay sesiones disponibles</span>
-                <img src="./src/NoSession.png" alt="NoSession">
-            </div>`;
+                <div class="o-nosession-content">
+                    <span>No hay sesiones disponibles</span>
+                    <img src="./src/NoSession.png" alt="NoSession">
+                </div>`;
         }
         else {
             let session = data[0];
-            let lastCurrentSession = sessionStorage.getItem('currentSession');
-            if(lastCurrentSession == null || lastCurrentSession._id !== session._id){
+            if (sessionStorage.getItem('currentSession') == null) {
+                //si no hay sesiones en curso
                 $('#mainContainer').load('deteccion.html', _ => {
-                    //si no hay una sesion actual anterior o la sesion anterior es diferente a la actual
                     document.getElementById("titleDetection").innerHTML = session.NombreCurso;
                     document.getElementById("groupDetection").innerHTML = `Grupo ${session.NumeroGrupo}`;
                     document.getElementById('sessionDuration').innerHTML = `${timeFormatter(new Date(session.HoraInicio))}&nbsp--&nbsp${timeFormatter(new Date(session.HoraFinal))}`;
                     sessionStorage.setItem('currentSession', JSON.stringify(session));
+                    //seteo un timer para actualizar la pagina cuando haya acabado la sesion
+                    let displacement = 0;
+                    setTimeout(_ => {
+                        eel.stopTransmition()(_ => {
+                            //remover la ultima sesion de la memoria
+                            sessionStorage.removeItem('currentSession');
+                            reloadSessions();
+                        })
+                    }, Math.abs(new Date(session.HoraFinal).getTime() - new Date().getTime()) + displacement);
                     getDevices();
                     eel.startTransmition(false);
                 })
@@ -36,33 +44,38 @@ function getCurrentSession(user) {
  * @param {object} user 
  */
 function getUpcomingSessions(user) {
+    document.getElementById('sessionsPlaceholder').innerHTML = "";
     let url = 'https://classmood-appserver.herokuapp.com/upcoming'
     $.get(url, { Correo: user.Correo }, (data) => {
         if (data.length === 0) {
-            $("#nextSessionsContainer").append('<span class="o-no-next-sessions">No hay sesiones agendadas</span>')
+            $("#sessionsPlaceholder").append('<span class="o-no-next-sessions">No hay sesiones agendadas</span>');
         }
         else {
-            let lastUpcomingSessions = sessionStorage.getItem('lastUpcomingSessions');
-            let upcomingSessions = data.map(session => session._id);
-            console.log(upcomingSessions)
-            if(lastUpcomingSessions == null){
-                for (session of data) {
-                    $("#nextSessionsContainer").append(`
-                    <div class="o-session">
-                        <div class="o-session-header">
-                            <span class="o-session-title" id="sessionGroupName">${session.NombreCurso}</span>
-                            <span class="o-session-group" id="sessionGroupNumber">Grupo ${session.NumeroGrupo}</span>
-                        </div>
-                        <div class="o-upsession-time">
-                            <span id="sessionStartTime">${timeFormatter(new Date(session.HoraInicio))}</span>&nbsp--&nbsp
-                            <span id="sessioneNDTime">${timeFormatter(new Date(session.HoraFinal))}</span>
-                        </div>
+            for (session of data) {
+                $("#sessionsPlaceholder").append(`
+                <div class="o-session">
+                    <div class="o-session-header">
+                        <span class="o-session-title" id="sessionGroupName">${session.NombreCurso}</span>
+                        <span class="o-session-group" id="sessionGroupNumber">Grupo ${session.NumeroGrupo}</span>
                     </div>
-                `)
-                }
+                    <div class="o-upsession-time">
+                        <span id="sessionStartTime">${timeFormatter(new Date(session.HoraInicio))}</span>&nbsp--&nbsp
+                        <span id="sessioneNDTime">${timeFormatter(new Date(session.HoraFinal))}</span>
+                    </div>
+                </div>`);
             }
         }
     })
+}
+
+/**
+ * funcion para recargar las sesiones
+ */
+function reloadSessions() {
+    console.log('RECARGAAAAAR');
+    let user = JSON.parse(sessionStorage.getItem("user"));
+    getUpcomingSessions(user);
+    getCurrentSession(user);
 }
 
 //obtener el usuario
